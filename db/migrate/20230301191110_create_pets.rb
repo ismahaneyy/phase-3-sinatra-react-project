8264e10df315719040344c9bb44ffa00bf321a8a
ismahaneyy <liciousmayrah@gmail.com>
2023-03-01 22:13:00 +0300

    added pets table

--- db/migrate/20230301191110_create_pets.rb
+++ db/migrate/20230301191110_create_pets.rb
@@ -1,4 +1,10 @@
 class CreatePets < ActiveRecord::Migration[6.1]
   def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :breed
+      t.string :image
+      t.integer :age 
+    end 
   end
 end
