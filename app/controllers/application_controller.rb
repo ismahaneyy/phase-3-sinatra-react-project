14111b15492d226f8d890d58640869f945297f9a
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 00:21:52 +0300

    it defines a route handler for a POST request to the /users endpoint of a web API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < AppController
   set :default_content_type, 'application/json'
   
- 
+  post '/users' do
+    user_data = JSON.parse(request.body.read)
+    user = User.find_by(phone: user_data['phone'])
+    
+    if user
+      status 400
+      body 'User already exists. Please login'
+    else
+      new_user = User.create(user_data)
+      new_user.to_json
+    end
+  end
 
 end
