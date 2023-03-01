066367ee6f152138315557dad577dac2b997c343
ismahaneyy <liciousmayrah@gmail.com>
2023-03-01 22:13:53 +0300

    added pets migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_190704) do
+ActiveRecord::Schema.define(version: 2023_03_01_191110) do
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.string "image"
+    t.integer "age"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
