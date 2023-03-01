715b2e347ab5ed1ccc688e64d766906a6f95bfcb
ismahaneyy <liciousmayrah@gmail.com>
2023-03-01 22:34:23 +0300

    added an instance method authenticate that allows you to authenticate a user based on their phone number and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
     has_many :pets
+    
+    def self.authenticate(phone, password)
+        user = find_by(phone: phone)
+        return user if user&.authenticate(password)
+    
+        user.to_json 
+      end 
+  
 end 
