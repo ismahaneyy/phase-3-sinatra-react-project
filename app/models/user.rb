cb0871cc0c46f937ba95059c21f3b6871f2961fa
ismahaneyy <liciousmayrah@gmail.com>
2023-03-01 22:38:32 +0300

    added instance confirm_user_and_pass that  allows you to confirm a user's email and password. If the user exists and the provided password is correct, it returns the user object, otherwise it returns false

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ActiveRecord::Base
     def self.create_new_user(name, email, phone_number)
       User.create(name: name, email: email, phone_number: phone_number)
     end 
+
+    def confirm_user_and_pass(email, password)
+      user = User.find_by(email: email)
+      user && user.authenticate(password)
+    end 
 end 
