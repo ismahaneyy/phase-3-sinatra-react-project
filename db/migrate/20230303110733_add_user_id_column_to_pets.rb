17e68ad1e2f30db3fab4105b6c750def569910f3
ismahaneyy <liciousmayrah@gmail.com>
2023-03-03 14:13:56 +0300

    updated seed

--- db/migrate/20230303110733_add_user_id_column_to_pets.rb
+++ db/migrate/20230303110733_add_user_id_column_to_pets.rb
@@ -1,4 +1,4 @@
-class AddUserIdColumnToPets < ActiveRecord::Migration[6.1]
+class AddUserIdColumnToPets < ActiveRecord::Migration[7.0]
   def change
     add_column :pets, :user_id, :integer 
   end
