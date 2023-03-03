d818fd18e7fb706ee913ee0d701d1575a2635d39
ismahaneyy <liciousmayrah@gmail.com>
2023-03-03 13:45:32 +0300

    commented out codes in user and pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,29 +1,29 @@
 class Pet < ActiveRecord::Base
     belongs_to :user
 
-    def personal_pets
-      user.pets
-    end 
+    # def personal_pets
+    #   user.pets
+    # end 
    
-     def self.all_pets
-      all.map do |pet|
-        {
-          name: pet.name,
-          age: pet.age,
-          image: pet.image
-        }
-      end 
-    end 
+    #  def self.all_pets
+    #   all.map do |pet|
+    #     {
+    #       name: pet.name,
+    #       age: pet.age,
+    #       image: pet.image
+    #     }
+    #   end 
+    # end 
 
-    def self.find_by_name_or_breed(search)
-      where("name ILIKE ? OR breed ILIKE ?", "%#{search}%", "%#{search}%")
-    end 
+    # def self.find_by_name_or_breed(search)
+    #   where("name ILIKE ? OR breed ILIKE ?", "%#{search}%", "%#{search}%")
+    # end 
     
-    def update_pet_details(attributes)
-      update(attributes)
-    end 
+    # def update_pet_details(attributes)
+    #   update(attributes)
+    # end 
 
-    def remove_pet_details
-      destroy
-    end 
+    # def remove_pet_details
+    #   destroy
+    # end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
     has_many :pets
     
-    def self.authenticate(phone, password)
-        user = find_by(phone: phone)
-        return user if user&.authenticate(password)
+    # def self.authenticate(phone, password)
+    #     user = find_by(phone: phone)
+    #     return user if user&.authenticate(password)
     
-        user.to_json 
-    end 
+    #     user.to_json 
+    # end 
     
-    def self.create_new_user(name, email, phone_number)
-      User.create(name: name, email: email, phone_number: phone_number)
-    end 
+    # def self.create_new_user(name, email, phone_number)
+    #   User.create(name: name, email: email, phone_number: phone_number)
+    # end 
 
-    def confirm_user_and_pass(email, password)
-      user = User.find_by(email: email)
-      user && user.authenticate(password)
-    end 
+    # def confirm_user_and_pass(email, password)
+    #   user = User.find_by(email: email)
+    #   user && user.authenticate(password)
+    # end 
 end 
