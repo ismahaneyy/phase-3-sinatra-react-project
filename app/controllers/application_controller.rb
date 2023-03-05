5fff4f0c353c7483f8ea0644862e0718c6ea5e4e
ismahaneyy <liciousmayrah@gmail.com>
2023-03-05 08:36:59 +0300

    updated application contoller and changed migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,83 +1,62 @@
-class ApplicationController < AppController
-  # User routes
-  # it works
-  post '/users' do
-    user_data = JSON.parse(request.body.read)
-    user = User.find_by(phone: user_data['phone'])
-    
-    if user
-      status 400
-      { data: { success: false, error: 'User already exists. Please login!'} }.to_json
-    else
-      new_user = User.create(user_data)
-      new_user.to_json
-      { success: true, user: new_user }.to_json
-    end
+class ApplicationController < Sinatra::Base 
+  set :default_content_type, 'application/json'
+
+  configure do 
+    enable :cross_origin
+  end
+
+    # returns all the users
+  get "/" do 
+    all_users = User.all
+    all_users.to_json
   end
   
-  post '/login' do
-    request.body.rewind
-    request_payload = JSON.parse(request.body.read)
-  
-    phone = request_payload['phone']
-    password = request_payload['password']
-  
-    user = User.find_by(phone: phone, password: password)
-    if user
-      { success: true, user: user }.to_json
-    else
-      { success: false, error: 'Invalid credentials' }.to_json
-    end
+   #returns all the pets for a specific user
+   get "/pets/:username" do
+    single_user = User.find_by(username:params[:username])
+    single_user.pets.to_json 
   end
-  
+   
+    #Searches through the list of all the pets and returns the pets that match
+    post '/pets/search_all' do
+      search_request = JSON.parse(request.body.read)
+      search_response = Pet.where('name LIKE ? OR breed LIKE ?', "%#{body['query']}%", "%#{body['query']}%")
+      search_response.to_json
+    end 
 
-# Pet routes
-# it works
-post '/pets/create' do
-    begin
-        new_pet = Pet.create(JSON.parse(request.body.read))
-        json_response(code: 201, data: new_pet)
-    rescue => e 
-        json_response(code: 422, data: e.message)
-    end 
-end
-# technically works
-get '/users/:user_id/pets' do
-  user = User.find(params[:user_id])
-  user.pets.to_json 
-end
-# it works
-get '/pets' do
-  Pet.all.to_json
-end
-# it works
-post '/pets/search' do
-    body = JSON.parse(request.body.read)
-    pets = Pet.where('name LIKE ? OR breed LIKE ?', "%#{body['query']}%", "%#{body['query']}%")
-    pets.to_json
-end 
+      #Searches through a list of all the pets of a speific user and returns the pets that match
+  post '/pets/search/:username' do
+    search_request = JSON.parse(request.body.read)
+    my_pets = User.find_by(username:params[:username])
+    search_response = my_pets.pets.where('name LIKE ? OR breed LIKE ?', "%#{body['query']}%", "%#{body['query']}%")
+    search_response.to_json
+  end 
 
-  # it works
-  post '/update/pets/:id' do
-    begin 
-        data = JSON.parse(request.body.read)
-        pet = Pet.find(params[:id])
-        pet.update(data)
-        pet.to_json
-    rescue => e 
-        { error: e.message}
-    end 
-end
-# it works
-delete '/delete/pets/:id' do 
-    begin
-        # remove = JSON.parse(request.body.read) 
-        pet = Pet.find(params[:id])
-        pet.destroy
-        # status 204 
-        { success: true, message: "Removed successfully"  }.to_json
-    rescue => e 
-        { error: e.message}
-    end 
-end
+     #returns a list of all the pets in the database
+     get "/pets"  do
+      all_pets = Pet.all
+      all_pets.to_json
+    end
+
+      #adds a new pet into the database
+  post "/pets" do
+    new_pet = Pet.create(JSON.parse(request.body.read))
+  end
+
+    #removes a pet from the database
+    delete "/pets/:id" do
+      find_pet = Pet.find(params[:id])
+      find_pet.destroy
+    end
+      #changes the details of a single pet
+  put "/pets/:id" do 
+    new_details = JSON.parse(request.body.read)
+        find_pet = Pet.find(params[:id])
+        find_pet.update(data)
+        find_pet.to_json
+  end
+
+  post "/user" do
+    new_user = User.create(JSON.parse(request.body.read))
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_120410) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_051622) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
-    t.string "image"
-    t.integer "age"
+    t.string "image_url"
     t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "username"
     t.string "password"
-    t.integer "phone"
   end
 
 end
